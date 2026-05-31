3763d7aecad3ff0fcca40039477a9aef8db3baad
F4RAN <parsa.hassani30@gmail.com>
2026-05-31 10:03:40 +0330

    feat: Add 15_channels

---
+++ 15_channels/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+	// // 1. Unbuffered channel
+	ch1 := make(chan int)
+	go func() { ch1 <- 1 }()
+	fmt.Println(<-ch1)
+
+	// 2. Buffered channel
+	ch2 := make(chan int, 2)
+	ch2 <- 2
+	fmt.Println(<-ch2)
+	ch2 <- 3
+	fmt.Println(<-ch2)
+
+	// // 3. Send-only channel
+	ch3 := make(chan int)
+	go func(ch chan<- int) { ch <- 4 }(ch3) // Expanded version is at the end (sendOnly func)
+	fmt.Println(<-ch3)
+
+	// // 4. Receive-only channel
+	ch4 := make(chan int, 1)
+	ch4 <- 5
+	func(ch <-chan int) { // Expanded version is at the end (receiveOnly func)
+		fmt.Println(<-ch)
+	}(ch4)
+
+	// // 5. Nil channel
+	var ch5 chan int
+	fmt.Println(ch5 == nil)
+
+	// // 6. Closed channel
+	ch6 := make(chan int, 1)
+	ch6 <- 6
+	close(ch6)
+
+	value, ok := <-ch6
+	fmt.Println("closed channel value:", value, "ok:", ok)
+
+	value, ok = <-ch6
+	fmt.Println("closed channel after empty:", value, "ok:", ok)
+
+	// // 7. Channel of channels
+	ch7 := make(chan chan int)
+	go func() {
+		inner := make(chan int, 1)
+		inner <- 7
+		ch7 <- inner
+	}()
+	fmt.Println(<-(<-ch7))
+}
+
+// EXAMPLE OF Send Only and Receive Only go routines
+// func sendOnly(ch chan<- int) {
+// 	ch <- 100
+// }
+
+// func receiveOnly(ch <-chan int) {
+// 	value := <-ch
+// 	fmt.Println("receive-only channel:", value)
+// }
